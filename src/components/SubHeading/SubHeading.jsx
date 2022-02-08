6a51190d1f692c46c84009a079be7456a5a07789
tatsuma-sato <t.tsuma.s@gmail.com>
2022-02-07 23:30:12 -0800

    added header and aboutus

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import images from "../../constants/images";
 
-const SubHeading = () => (
-  <div>
-    SubHeading
-  </div>
-);
+const SubHeading = ({ title }) => {
+
+  return (
+    <div style={{ marginBottom: "1rem" }}>
+      <p className="p__cormorant">{title}</p>
+      <img src={images.spoon} alt="Spoon" />
+    </div>
+  );
+};
 
 export default SubHeading;
